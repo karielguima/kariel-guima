8520f9257aea792848d941e8ef055ba47f26736e
karielguima@gmail.com <karielguima@gmail.com@180f9adc-1447-d191-5c91-bf1cf903f70e>
2014-09-09 12:05:29 +0000

    qweerw

---
+++ trunk/Cliente.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package caixaeletronico;
+
+/**
+ *
+ * @author Aluno
+ */
+class Cliente {
+    String Nome ,Cpf , Data;
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getCpf() {
+        return Cpf;
+    }
+
+    public void setCpf(String Cpf) {
+        this.Cpf = Cpf;
+    }
+
+    public String getData() {
+        return Data;
+    }
+
+    public void setData(String Data) {
+        this.Data = Data;
+    }
+    
+    
+    
+}
